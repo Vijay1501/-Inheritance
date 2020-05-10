4083c96f5dbc15ca29cae71641f46893c3049480
Vijay1501 <62753763+Vijay1501@users.noreply.github.com>
2020-05-10 11:18:58 +0530

    Add files via upload

---
+++ Raindrop.js
@@ -0,0 +1,19 @@
+class Raindrop{
+    constructor(){
+        this.x=random(0,400)
+        this.y=random(3,400)
+}
+update(){
+    this.y=this.y+5
+    if(this.y>400){
+        this.y=random(0,100)
+        this.x=random(0,400)
+    }
+    }
+    display(){
+    push()
+    strokeWeight(1)
+    stroke("red")
+    line(this.x,this.y,this.x,this.y+15)
+    }
+}
